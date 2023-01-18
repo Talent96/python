8e1eb5b2067457c1e96ea685208c5d4f10bf8f10
Mark Memga <askofmark@gmail.com>
2023-01-18 23:03:12 +0100

    first commit

---
+++ quiz.py
@@ -0,0 +1,28 @@
+
+print("What is the sum of 20 and 10?")
+answer = input("Answer")
+if answer == "30":
+	print("Correct")
+else: 
+	print("Try again") 
+
+print("What is the product of 5 and 4")
+answer = input("Answer")
+if answer == "20":
+	print("Correct")
+else: 
+	print("Try again")
+
+print("What is the remainder when 11 is divided by 5?")
+answer = input("Answer")
+if answer == "1":
+	print("Correct")
+else: 
+	print("Try again")
+
+print("What is 5 + 2 * 2?")
+answer = input("Answer")
+if answer == "9":
+	print("Correct")
+else: 
+	print("Try again")
