8cc9f96f8dc46b730b70ee0ef320ae40e4239868
Talent96 <121486742+Talent96@users.noreply.github.com>
2023-01-23 08:36:35 +0100

    Update quiz2.py

--- quiz2.py
+++ quiz2.py
@@ -1,3 +1,9 @@
+#display question
+#prompt user to put answer
+#compare the user inputed answer with the right answer
+#display correct if user enters the right answer otherwise display wrong, try again
+#do that for five questions.
+
 print("Welcome to this simple maths quiz, let's begin..")
 
 for x in range (20): 
