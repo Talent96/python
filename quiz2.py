f8c5062d05ebe99c9ef0ebdbb55f2ab99a054500
Talent96 <121486742+Talent96@users.noreply.github.com>
2023-01-23 08:57:35 +0100

    Update quiz2.py

--- quiz2.py
+++ quiz2.py
@@ -1,138 +1,135 @@
-#display question
-#prompt user to put answer
-#compare the user inputed answer with the right answer
-#display correct if user enters the right answer otherwise display wrong, try again
-#do that for five questions.
-
 print("Welcome to this simple maths quiz, let's begin..")
 
-for x in range (20): 
-	print("What is the sum of 20 and 10?")
-	answer = input("Answer: ")
-	if answer == "30":
-		print("Correct")
-	else: 
-		print("Wrong, Try again") 
+user_choice = int(input("Enter 1 to start, 0 to exit "))
+while user_choice == 1:
 
-	print("What is the product of 5 and 4")
-	answer = input("Answer: ")
-	if answer == "20":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
+	for x in range (20): 
+		print("What is the sum of 20 and 10?")
+		answer = input("Answer: ")
+		if answer == "30":
+			print("Correct")
+		else: 
+			print("Wrong, Try again") 
 
-	print("What is the remainder when 11 is divided by 5?")
-	answer = input("Answer: ")
-	if answer == "1":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
+		print("What is the product of 5 and 4")
+		answer = input("Answer: ")
+		if answer == "20":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
 
-	print("What is 5 + 2 * 2?")
-	answer = input("Answer: ")
-	if answer == "9":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
+		print("What is the remainder when 11 is divided by 5?")
+		answer = input("Answer: ")
+		if answer == "1":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
 
-	print("what is the range between 2 and 10")
-	answer = input("Answer is : ")
-	if answer == "8":
-		print("correct")
-	else: 
-		print("Wrong, try again")
-	print("What is 5 * 10?")
-	answer = input("Answer: ")
-	if answer == "50":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 6 * 6?")
-	answer = input("Answer: ")
-	if answer == "36":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 2**2?")
-	answer = input("Answer: ")
-	if answer == "4":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 100 / 5?")
-	answer = input("Answer: ")
-	if answer == "20":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 60 - 30?")
-	answer = input("Answer: ")
-	if answer == "30":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 1 ** 1?")
-	answer = input("Answer: ")
-	if answer == "1":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 5 % 5?")
-	answer = input("Answer: ")
-	if answer == "0":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 4 + 8 - 6?")
-	answer = input("Answer: ")
-	if answer == "6":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 40 / 4?")
-	answer = input("Answer: ")
-	if answer == "10":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 9 + 3?")
-	answer = input("Answer: ")
-	if answer == "12":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 2 * 0?")
-	answer = input("Answer: ")
-	if answer == "0":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 190 - 70?")
-	answer = input("Answer: ")
-	if answer == "120":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 5 * 2 + 2?")
-	answer = input("Answer: ")
-	if answer == "12":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 4 / 4?")
-	answer = input("Answer: ")
-	if answer == "1":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("What is 3 * 3?")
-	answer = input("Answer: ")
-	if answer == "9":
-		print("Correct")
-	else: 
-		print("Wrong, Try again")
-	print("Thanks for participating")
-	break
+		print("What is 5 + 2 * 2?")
+		answer = input("Answer: ")
+		if answer == "9":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+
+		print("what is the range between 2 and 10")
+		answer = input("Answer is : ")
+		if answer == "8":
+			print("correct")
+		else: 
+			print("Wrong, try again")
+		print("What is 5 * 10?")
+		answer = input("Answer: ")
+		if answer == "50":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 6 * 6?")
+		answer = input("Answer: ")
+		if answer == "36":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 2**2?")
+		answer = input("Answer: ")
+		if answer == "4":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 100 / 5?")
+		answer = input("Answer: ")
+		if answer == "20":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 60 - 30?")
+		answer = input("Answer: ")
+		if answer == "30":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 1 ** 1?")
+		answer = input("Answer: ")
+		if answer == "1":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 5 % 5?")
+		answer = input("Answer: ")
+		if answer == "0":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 4 + 8 - 6?")
+		answer = input("Answer: ")
+		if answer == "6":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 40 / 4?")
+		answer = input("Answer: ")
+		if answer == "10":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 9 + 3?")
+		answer = input("Answer: ")
+		if answer == "12":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 2 * 0?")
+		answer = input("Answer: ")
+		if answer == "0":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 190 - 70?")
+		answer = input("Answer: ")
+		if answer == "120":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 5 * 2 + 2?")
+		answer = input("Answer: ")
+		if answer == "12":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 4 / 4?")
+		answer = input("Answer: ")
+		if answer == "1":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("What is 3 * 3?")
+		answer = input("Answer: ")
+		if answer == "9":
+			print("Correct")
+		else: 
+			print("Wrong, Try again")
+		print("Thank you for participating")
+		user_choice = int(input("Do you wish to continue?, press 1 to continue or 0 to exit"))
  
 	
 
